f5dd5cf424df6036a31b7755f4cf447f559f20e5
steph <sjapodaca1103@gmail.com>
2021-10-16 21:43:10 -0700

    fourth

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,8 @@ $("#currentDay").html(todaysDate);
 $(document).ready(function(){
     $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
-        var time = $(this).parent().alert("id");
+        // var time = $(this).parent().alert("id");
+        var time = $(this).parent("id").alert();
 
         localStorage.setItem(time, text);
 
@@ -12,6 +13,43 @@ $(document).ready(function(){
 
     function timeTracker() {
         var timeNow = moment ().hour();
-        
+
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime === timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
+
+            else if (blockTime < timeNow) {
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+                $(this).addClass("past");
+
+            }
+
+            else {
+                $(this).removeClass("fpresent");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+
     }
+
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour1 .description").val(localStorage.getItem("hour1"));
+    $("#hour2 .description").val(localStorage.getItem("hour2"));
+    $("#hour3 .description").val(localStorage.getItem("hour3"));
+    $("#hour4 .description").val(localStorage.getItem("hour4"));
+    $("#hour5 .description").val(localStorage.getItem("hour5"));
+
+    timeTracker();
+
 })
